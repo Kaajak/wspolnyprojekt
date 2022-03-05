d66436552e52b825b2d6c0e046fc27494f63dedd
Kaajak <95999361+Kaajak@users.noreply.github.com>
2022-03-05 15:08:57 +0100

    Create inwestyjce.py

---
+++ inwestyjce.py
@@ -0,0 +1,9 @@
+def investments(USD):
+    if USD >= 100000:
+        return("Invest in gold")
+    if USD >= 100000:
+        return("Invest in diamonds")
+    if USD >= 10000000:
+        return("Invest in palacyki")
+    else:
+        return("Don't even try, beggar")
